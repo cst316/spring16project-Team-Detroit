170f988e1baaf1e43312d00203b37281c22d3f3e
Ryan Schultz <reschul1@asu.edu>
2016-02-13 21:47:25 -0500

    Added missing unit test to test suite

--- src/net/sf/memoranda/unittest/EmailAndContactTestSuite.java
+++ src/net/sf/memoranda/unittest/EmailAndContactTestSuite.java
@@ -4,8 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import net.sf.memoranda.util.SerializationUtil;
+
 @RunWith(Suite.class)
-@SuiteClasses({ ContactListStorageTest.class, ContactListTest.class, DailyEmailTest.class, EmailContactTest.class, EmailerTest.class,})
+@SuiteClasses({ ContactListStorageTest.class, ContactListTest.class, DailyEmailTest.class, EmailContactTest.class, EmailerTest.class, SerializationUtil.class })
 public class EmailAndContactTestSuite {
 
 }
