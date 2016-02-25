74d82918cbba5c5e33075fe23a2586d3f77dc862
Casey <cfroke@asu.edu>
2016-02-24 22:11:11 -0800

    Adding New Java Classes to Branch.

---
+++ src/net/sf/memoranda/Login.java
@@ -0,0 +1,54 @@
+package net.sf.memoranda;
+
+import net.sf.memoranda.ui.LoginDialogBox;
+import net.sf.memoranda.ui.FirstTimeLoginBox;
+import net.sf.memoranda.util.Configuration;
+
+public class Login {
+	
+	static boolean cancelled = false;
+	static boolean valid = true; 
+	static boolean loginOpen = false;
+
+	public static void getDetails(){
+		if (Configuration.get("USER_PASSWORD").toString().equalsIgnoreCase("default")||
+				Configuration.get("USER_PASSWORD") == "")
+		{
+			loginOpen = true;
+			new FirstTimeLoginBox();
+		}
+		else if (Configuration.get("USER_PASSWORD").toString().equalsIgnoreCase("none"))
+		{
+			cancelled = true;
+		}
+		else
+		{
+			loginOpen = true;
+			new LoginDialogBox();
+		}
+	}
+	
+	public static boolean isOpen()
+	{
+		return loginOpen;
+	}
+	
+	public static boolean isValid()
+	{
+		return valid;
+	}
+	
+	public static boolean isCancelled()
+	{
+		return cancelled;
+	}
+	
+	public static void cancelled()
+	{
+		cancelled = true;
+	}
+	public static void invalid()
+	{
+		valid = false; 
+	}
+}
